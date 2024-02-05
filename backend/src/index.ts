a3bb7a571c65208a54bea655a3fe5845290a092b
Jorgen Bengtsson <jorgen@pm-production.nu>
2024-02-05 10:57:19 +0100

    Comments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -51,22 +51,34 @@ app.post("/api/products", (req, res) => {
 
 app.get("/getProducts", async (req, res) => {
   try {
+    // Launch a browser through Puppeteer
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
+
+    // Open Target Website
     await page.goto(targetUrl);
+
+    // Type Username and Password (Assume that the login screen is the first we "see")
     await page.type("#username", loginUser.uid);
     await page.type("#password", loginUser.pwd);
+
+    // Submit the login form and wait until the application has responded
     await Promise.all([
       page.click("#submitLogin"),
       page.waitForNavigation({ waitUntil: "networkidle0" }),
     ]);
+
+    // Wait so that the list of Products are displayed, then Scrape the html code for the list of products
     await page.waitForSelector("#products");
     let value = await page.$eval("#products", (element) => {
       return element.innerHTML;
     });
+
+    // Take a screenshot as proof :)
     await page.screenshot({ path: "products-screenshot.png" });
+
+    // Clean up
     await browser.close();
-    console.log(value);
     res.send(value);
   } catch (error) {
     console.log(error);
@@ -75,28 +87,49 @@ app.get("/getProducts", async (req, res) => {
 
 app.get("/addProduct", async (req, res) => {
   try {
+    // Launch a browser through Puppeteer
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
+
+    // Open Target Website
     await page.goto(targetUrl);
+
+    // Type Username and Password (Assume that the login screen is the first we "see")
     await page.type("#username", loginUser.uid);
     await page.type("#password", loginUser.pwd);
+
+    // Submit the login form and wait until the application has responded
     await Promise.all([
       page.click("#submitLogin"),
       page.waitForNavigation({ waitUntil: "networkidle0" }),
     ]);
+
+    // Navigate to Register page/form
     await Promise.all([
       page.click("#register"),
       page.waitForNavigation({ waitUntil: "networkidle0" }),
     ]);
+
+    // Wait for the Title element on the page to be displayed
     await page.waitForSelector("#productName");
+
+    // Type in ProductName and Number (random strings)
     await page.type("#productname", makeString(6));
     await page.type("#productnumber", makeString(9));
+
+    // Take a proof screenshot
     await page.screenshot({ path: "register-screenshot.png" });
+
+    // Submit the Register form
     await Promise.all([
       page.click("#submitRegister"),
       page.waitForNavigation({ waitUntil: "networkidle0" }),
     ]);
+
+    // Wait for a page element, again
     await page.waitForSelector("#productName");
+
+    // Clean up
     browser.close();
     res.send("Product added");
   } catch (error) {
